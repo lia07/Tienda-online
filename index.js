f558038b5af4539ab7c7cdf92dea0597247f7a0c
lia07 <sarzurisilvestrelss@gmail.com>
2023-03-13 08:47:33 -0400

    [UPD]

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const btnCart = document.querySelector('container-icon');
+const btnCart = document.querySelector('container-cart-icon');
 
 const containerCartProducts = document.querySelector(('container-cart-products hidden-cart'));
 
 btnCart.addEventListener('click', () => {
     containerCartProducts.classList.toggle('hidden-cart')
 })
+
+
+/*=====================================*/
+const cartInfo = document.querySelector('cart-product')
